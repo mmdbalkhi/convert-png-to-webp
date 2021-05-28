44c1c37bc2ff3534b8e61c39b38e5bcc4a248c9a
Komeil Parseh <65954744+KomeilParseh@users.noreply.github.com>
2021-05-28 16:19:04 +0430

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
 from hashlib import sha256
+from os import system, listdir
 
+dir = listdir()
 
-with open("sample.png", "r") as f:
+new = ""
+num = 0
+
+for inputed in dir:
+    new = str(inputed).replace(".png", "")
+    system(f"cwebp -q 80 {new}.png -o {new}.webp")
+    print(new, "converted")
+    num += 1
+print(num, "convert(s) successful")
+if yesorno == "yes":
+    system("rm *.png")
+    print("remove successful.\n\t goodluck!")
+
+with open("sample.webp", "r") as f:
     for chunk in iter(lambda: f.read(4096), b""):
         sha256.update(chunk)
     hash = hash_md5.hexdigest()
